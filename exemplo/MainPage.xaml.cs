d7db007964c6770741aefc42cf0caa092c9d74b4
ChicoNarciso <narcisofrancisco971@gmail.com>
2024-03-06 18:18:58 -0300

    feat: Adiciona Enviar Mensagem

---
+++ exemplo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace exemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string Name = "";
+        //Método Construtor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce (CounterBtn.Text);
+        }
+        private void OnMessageClicked(object sender, EventArgs e)
+        {
+            Name = NameEntry.Text;
+            MessageLabel.Text = $"Hello,{Name}!";
+        }
+
+    }
+
+}
